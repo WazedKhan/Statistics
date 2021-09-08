8270a5ef0b16fb4e1bf1bcf64bf71a68cef41913
Wazed Khan <wazedkhan111024@gmail.com>
2021-09-08 22:23:05 +0600

    Added std for sample and population

--- Standar_deviation.py
+++ Standar_deviation.py
@@ -1,14 +1,28 @@
 import math
 
-def variance(data):
+# variance for population
+def varianceP(data):
       xminus_mean = 0
       mean = sum(data)/len(data)
       for i in data:
             xminus_mean += math.pow((i - mean), 2)
       return xminus_mean/len(data)
 
-pop = [1,2,3,8,7]
-value = variance(pop)
+#variance for sample
+def varianceS(data):
+      xminus_mean = 0
+      mean = sum(data)/len(data)
+      for i in data:
+            xminus_mean += math.pow((i - mean), 2)
+      return xminus_mean/(len(data)-1)
 
-std = math.sqrt(value)
-print(std)
+pop = [1,2,3,8,7]
+value_of_pop_var = varianceP(pop)
+
+std_p = math.sqrt(value_of_pop_var)
+print(std_p)
+
+value_of_sam_var = varianceS(pop)
+
+std_s = math.sqrt(value_of_sam_var)
+print(std_s)
